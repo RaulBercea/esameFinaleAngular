3d9fac30f98f7ee2c67ddb915ae284c7138fbb05
Raul Bercea <raulbercea@gmail.com>
2022-12-09 18:00:43 +0100

    changed the home and results page style

--- src/app/shared/resultsComponent/results.component.ts
+++ src/app/shared/resultsComponent/results.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { apiService } from 'src/app/services/api.service';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs';
 
 type dataseries = {
   cloudcover: number;
@@ -18,7 +19,6 @@ type dataseries = {
   selector: 'Results',
   templateUrl: './results.component.html',
 })
-
 export class Results implements OnInit {
   constructor(
     private apiService: apiService,
@@ -27,26 +27,36 @@ export class Results implements OnInit {
   ) {}
 
   sunRes: any = {
-    sunrise: "",
-    sunset: "",
+    sunrise: '',
+    sunset: '',
   };
-
 
   meteoRes: dataseries[] = [];
 
   ngOnInit(): void {
     this.route.params.subscribe((params) => {
-      this.apiService.getSunsetAndSunrise(params['lat'], params['long'])
-      .subscribe((res:any)=>{
-        this.sunRes.sunrise = res.results.sunrise;
-        this.sunRes.sunset = res.results.sunset;
-      });
+      this.apiService
+        .getSunsetAndSunrise(params['lat'], params['long'])
+        .subscribe((res: any) => {
+          this.sunRes.sunrise = res.results.sunrise;
+          this.sunRes.sunset = res.results.sunset;
+        });
 
-      this.apiService.getMeteo(params['lat'], params['long'])
-      .subscribe((res:any)=>{
-        console.log(res)
-        this.meteoRes = res.dataseries;
-      })
+      this.apiService
+        .getMeteo(params['lat'], params['long'])
+        .subscribe((res: any) => {
+          this.meteoRes = res.dataseries;
+
+          for (const data of this.meteoRes) {
+            if (data.timepoint > 24 && data.timepoint <= 48) {
+              data.timepoint -= 24;
+            } else if (data.timepoint > 48) {
+              data.timepoint -= 48;
+            }
+          }
+
+          console.log(this.meteoRes);
+        });
     });
   }
 }
