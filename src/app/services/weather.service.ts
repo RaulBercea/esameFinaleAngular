cf271ca7dbb4ffc7cc2c1784e257f45dab4063b7
RaulBercea <100358692+RaulBercea@users.noreply.github.com>
2022-12-27 19:58:06 +0100

    formatted with prettier

--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,72 +1,71 @@
-import { dataseries } from "../shared/resultsComponent/results.component";
+import { dataseries } from '../shared/resultsComponent/results.component';
 
 export const convertTimePoint = (data: dataseries) => {
-    if (data.timepoint >= 24) {
-      data.timepoint %= 24;
-    }
+  if (data.timepoint >= 24) {
+    data.timepoint %= 24;
   }
-
+};
 
 export const addWeatherIcons = (data: dataseries) => {
-    if (data.cloudcover <= '2') {
-      data.icon = 'bi bi-sun';
-    }
-
-    if (data.cloudcover <= '6') {
-      data.icon = 'bi bi-cloud-sun';
-    }
-
-    if (data.cloudcover == '7' && data.rh2m < 90) {
-      data.icon = 'bi bi-cloud';
-    }
-
-    if (data.cloudcover >= '8') {
-      data.icon = 'bi bi-clouds';
-    }
-
-    if (data.cloudcover >= '7' && data.rh2m > 90) {
-      data.icon = 'bi bi-cloud-fog';
-    }
-
-    if (data.prec_type == 'rain') {
-      data.icon = 'bi bi-cloud-rain';
-    }
-
-    if (data.prec_type == 'snow') {
-      data.icon = 'bi bi-snow';
-    }
+  if (data.cloudcover <= '2') {
+    data.icon = 'bi bi-sun';
   }
 
-export const cloudCoverToPercentage = (data:dataseries) => {
-    switch (data.cloudcover.toString()) {
-      case '1':
-        data.cloudcover = '0%-6%';
-        break;
-      case '2':
-        data.cloudcover = '6%-19%';
-        break;
-      case '3':
-        data.cloudcover = '19%-31%';
-        break;
-      case '4':
-        data.cloudcover = '31%-44%';
-        break;
-      case '5':
-        data.cloudcover = '44%-56%';
-        break;
-      case '6':
-        data.cloudcover = '56%-69%';
-        break;
-      case '7':
-        data.cloudcover = '69%-81%';
-        break;
-      case '8':
-        data.cloudcover = '81%-94%';
-        break;
-      case '9':
-        data.cloudcover = '94%-100%';
-        break;
-      default:
-        break;
-    }
+  if (data.cloudcover <= '6') {
+    data.icon = 'bi bi-cloud-sun';
   }
+
+  if (data.cloudcover == '7' && data.rh2m < 90) {
+    data.icon = 'bi bi-cloud';
+  }
+
+  if (data.cloudcover >= '8') {
+    data.icon = 'bi bi-clouds';
+  }
+
+  if (data.cloudcover >= '7' && data.rh2m > 90) {
+    data.icon = 'bi bi-cloud-fog';
+  }
+
+  if (data.prec_type == 'rain') {
+    data.icon = 'bi bi-cloud-rain';
+  }
+
+  if (data.prec_type == 'snow') {
+    data.icon = 'bi bi-snow';
+  }
+};
+
+export const cloudCoverToPercentage = (data: dataseries) => {
+  switch (data.cloudcover.toString()) {
+    case '1':
+      data.cloudcover = '0%-6%';
+      break;
+    case '2':
+      data.cloudcover = '6%-19%';
+      break;
+    case '3':
+      data.cloudcover = '19%-31%';
+      break;
+    case '4':
+      data.cloudcover = '31%-44%';
+      break;
+    case '5':
+      data.cloudcover = '44%-56%';
+      break;
+    case '6':
+      data.cloudcover = '56%-69%';
+      break;
+    case '7':
+      data.cloudcover = '69%-81%';
+      break;
+    case '8':
+      data.cloudcover = '81%-94%';
+      break;
+    case '9':
+      data.cloudcover = '94%-100%';
+      break;
+    default:
+      break;
+  }
+};
--- src/app/shared/homeComponent/home.component.ts
+++ src/app/shared/homeComponent/home.component.ts
@@ -1,22 +1,23 @@
 import { Component } from '@angular/core';
-import {Router} from "@angular/router"
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'Home',
   templateUrl: './home.component.html',
 })
 export class Home {
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
-  latitude = "";
-  lognitude = "";
+  latitude = '';
+  lognitude = '';
   error = false;
 
-  getWeatherData(){
-    if(this.latitude && this.lognitude){
-      this.router.navigate(['/results/' + this.latitude + '/' + this.lognitude])
-    }
-    else{
+  getWeatherData() {
+    if (this.latitude && this.lognitude) {
+      this.router.navigate([
+        '/results/' + this.latitude + '/' + this.lognitude,
+      ]);
+    } else {
       this.error = true;
     }
   }
